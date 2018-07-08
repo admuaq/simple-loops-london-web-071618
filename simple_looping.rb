3534e86fe1b55e33f8a86dccebedaa4cab3b1eb8
Adham Muhammad <admuaq@gmail.com>
2018-07-08 08:47:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,10 +15,10 @@ def loop_iterator(number_of_times)
   counter = 0
   
   loop do 
-  puts phrase
-  counter += 1
-  break if counter == number_of_times
-end
+    puts phrase
+    counter += 1
+    break if counter == number_of_times
+    end
 end
 
 def times_iterator(number_of_times)
