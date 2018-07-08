aa5acdf7e161742d249cab02059960d883ba4699
Adham Muhammad <admuaq@gmail.com>
2018-07-08 08:54:06 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -59,5 +59,5 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   for number_of_times in 1..number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
-end
+  end
 end
